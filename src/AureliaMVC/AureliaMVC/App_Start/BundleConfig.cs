c534a2f5ef3367b068b7441670de164f8460a818
Amit <amit.hansda@in.pwc.com>
2016-12-08 18:30:00 +0530

    Added some changes

--- src/AureliaMVC/AureliaMVC/App_Start/BundleConfig.cs
+++ src/AureliaMVC/AureliaMVC/App_Start/BundleConfig.cs
@@ -26,6 +26,7 @@ namespace AureliaMVC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/customCss").Include("~/Content/bootstrap-paper.css"));
         }
     }
 }
